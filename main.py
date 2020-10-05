7934cc22a3c5b1074f50b7e559ae409e4c8e5a15
Mussa Yousef <72311394+Kapoosh27@users.noreply.github.com>
2020-10-05 17:08:07 +0100

    Tutorial 1 (Part 2)

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 21:52:12 2020
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+@author: MussaYousef
+"""
 
+#Create an array of int ranging from 5-15
+import numpy as np
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+a = np.arange(5,16,1)
+print(a)
+#Create an array containing 7 evenly spaced numbers between 0 and 23
+b = np.arange(0,23,3)
+print(b)
 
+# Numpy has several routines for generating artificial data following a particular structure. Check this page for different types. And generate an artificial numpy array with values between -1 and 1 that follow a uniform data distribution.
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+c = np.random.uniform(-1,1,100)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(c)
+
+#Visualise the array in an histogram in matplotlib.
+
+import matplotlib.pyplot as plt
+
+print(plt.hist(c))
+
+## Calculate the Euclidean distance
+
+p=np.random.rand(10)
+
+q=np.random.rand(10)
+
+print(p,q)
+
+ed= np.sqrt((p[0]-q[0])**2+(p[1]-q[1])**2+(p[2]-q[2])**2+(p[3]-q[3])**2+(p[4]-q[4])**2)+(p[5]-q[5])**2+(p[6]-q[6])**2+(p[7]-q[7])**2+(p[8]-q[8])**2+(p[9]-q[9])**2
+
+print(ed)
